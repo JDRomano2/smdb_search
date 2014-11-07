1a29f5d0ff7ad1dc6edf595de1a933c47cca49ae
Joseph Romano <JDRomano2@gmail.com>
2014-11-07 17:03:46 -0500

    Added option parser and more writing

---
+++ lib/smdb_options.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+# See OptionParser documentation on ruby-doc.org: http://ruby-doc.org/stdlib-2.1.4/libdoc/optparse/rdoc/OptionParser.html
+
+require 'optparse'
+require 'optparse/time'
+require 'ostruct'
+
+class SMDB_options
+
+  # Return a structure describing the options:
+  def self.parse(args)
+    # First, set default options
+    # Options are collected in `options`
+    options = OpenStruct.new()
+    options.config = "/etc/my.cnf"
+    options.search_fields = {}
+
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: smdb_search.rb [options]"
+      
+      opts.separator ""
+      opts.separator "Specific options:"
+
+      #Mandatory
+      opts.on("-c", "--config-file [FILE]",
+              "Import MySQL config file for connecting to SemMedDB (default '/etc/my.cnf')") do |conf|
+        options.config = conf
+      end
+
+      opts.on("--cui [CUI]", String, "UMLS CUI or GO ID") do |cui|
+        options.search_fields[:cui] = cui
+      end
+
+      opts.on("--predicate [PREDICATE]", String, "A predicate that is indexed by SemMedDB") do |pred|
+        options.search_fields[:predicate] = pred
+      end
+
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+      end
+    end
+
+    opt_parser.parse!(args)
+    options
+  end
+
+end
--- smdb_search.rb
+++ smdb_search.rb
@@ -1,9 +1,75 @@
 #!/usr/bin/env ruby
 ###############################
-# smdb_search.rb
+# smdb_.rb
 # Search SemMedDB, given one or more IDs
 
 require 'rubygems'
 require 'mysql2'
+require 'optparse'
+require_relative 'lib/smdb_options.rb'
+require 'pp'
 
-client = Mysql2::Client.new(:host => "localhost", :username => "root")
+#parse options
+options = SMDB_options.parse(ARGV)
+
+#pp options
+#pp ARGV
+
+class SMDB_search
+
+  def initialize(options)
+    @client = Mysql2::Client.new(:default_file => "/etc/my.cnf", :database => "kb_semanticmedline")
+    @search_terms = options.search_fields
+    determine_search_type
+  end
+
+  def determine_search_type
+    case
+    when @search_terms.has_key?(:cui) && @search_terms.has_key?(:predicate)
+      get_cui_predication_matches
+    else
+      puts "Error!"
+    end
+  end
+
+  def get_cui_predication_matches
+    puts "we got here"
+    @cui = @search_terms[:cui]
+    @predicate = @search_terms[:predicate]
+    
+    # get CONCEPT_IDs
+    r = @client.query("SELECT * FROM CONCEPT WHERE CUI = \"#{@cui}\"")
+    @concept_ids = []
+    r.each { |x| @concept_ids.push(x["CONCEPT_ID"]) }
+    pp @concept_ids
+
+    # get CONCEPT_SEMTYPE_IDs
+    @concept_semtype_ids = []
+    @concept_ids.each do |cid|
+      r = @client.query("SELECT * FROM CONCEPT_SEMTYPE WHERE CONCEPT_ID = \"#{cid}\"")
+      r.each { |x| @concept_semtype_ids.push(x["CONCEPT_SEMTYPE_ID"]) }
+    end
+    pp @concept_semtype_ids
+
+    # get PREDICATION_IDs
+    @predication_ids = []
+    @concept_semtype_ids.each do |csid|
+      r = @client.query("SELECT * FROM PREDICATION_ARGUMENT WHERE CONCEPT_SEMTYPE_ID = \"#{csid}\"")
+      r.each { |x| @predication_ids.push(x["PREDICATION_ID"]) }
+    end
+    pp @predication_ids
+
+    # for each predication_id, see if it uses the desired predicate
+    @predication_ids_matching_predicate = []
+    @predication_ids.each do |pid|
+      r = @client.query("SELECT * FROM PREDICATION WHERE PREDICATION_ID = \"#{pid}\"")
+      r.each do |y|
+        @predication_ids_matching_predicate.push(y["PREDICATION_ID"]) if y["PREDICATE"] == @predicate
+      end
+    end
+    pp @predication_ids_matching_predicate
+  end
+
+end
+
+SMDB_search.new(options)
